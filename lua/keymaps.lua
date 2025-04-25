1f65301c86d5b4e2bec37506f5d22b8c3334acc3
Andrey Podshivalenko <APodshivalenko@alfabank.ru>
2025-04-25 20:42:59 +0300

    dap and MAP_KEY global func

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
+MAP_KEY = vim.keymap.set
+MAP_KEY_OPTS = { noremap = true, silent = true }
 
 local function getVisualSelection()
     vim.cmd('noau normal! "vy"')
@@ -10,7 +10,7 @@ end
 
 local function mapTelescopeNV(shortcut, telescopeFun)
     local actions = require("telescope.actions")
-    map("v", shortcut, function() telescopeFun({
+    MAP_KEY("v", shortcut, function() telescopeFun({
         default_text = getVisualSelection(),
         attach_mappings  = function(_)
             actions.close:replace(function(prompt_bufnr)
@@ -19,8 +19,8 @@ local function mapTelescopeNV(shortcut, telescopeFun)
             end)
             return true
         end
-    }) end, opts)
-    map("n", shortcut, telescopeFun, opts)
+    }) end, MAP_KEY_OPTS)
+    MAP_KEY("n", shortcut, telescopeFun, MAP_KEY_OPTS)
 end
 
 local function current_dir()
@@ -28,51 +28,51 @@ local function current_dir()
     if dir == nil then return vim.fn.fnamemodify(vim.fn.bufname(), ":p:h") else return dir end
 end
 
-map("n", "<A-BS>", "<cmd>on<cr>", opts)
+MAP_KEY("n", "<A-BS>", "<cmd>on<cr>", MAP_KEY_OPTS)
 
 -- Split actions for undo
-map("i", "<Cr>", "<Cr><C-g>u", opts)
-map("i", "<Space>", "<Space><C-g>u", opts)
+MAP_KEY("i", "<Cr>", "<Cr><C-g>u", MAP_KEY_OPTS)
+MAP_KEY("i", "<Space>", "<Space><C-g>u", MAP_KEY_OPTS)
 
 -- Git
-map('n', '<A-C-a>', '<cmd>!git add %<cr>', { silent = false })
-map('n', '<leader>kk', '<cmd>vertical Git<cr>', opts)
-map('n', '<leader>kq', '<cmd>Git difftool<cr>', opts)
+MAP_KEY('n', '<A-C-a>', '<cmd>!git add %<cr>', { silent = false })
+MAP_KEY('n', '<leader>kk', '<cmd>vertical Git<cr>', MAP_KEY_OPTS)
+MAP_KEY('n', '<leader>kq', '<cmd>Git difftool<cr>', MAP_KEY_OPTS)
 
 -- Moving lines
-map("n", "<S-A-j>", "<cmd>silent! m .+1<CR>==", opts)
-map("n", "<S-A-k>", "<cmd>silent! m .-2<CR>==", opts)
-map("v", "<S-A-j>", ":<C-u>silent! '<,'>m '>+1<CR>gv", opts)
-map("v", "<S-A-k>", ":<C-u>silent! '<,'>m '<-2<CR>gv", opts)
+MAP_KEY("n", "<S-A-j>", "<cmd>silent! m .+1<CR>==", MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-k>", "<cmd>silent! m .-2<CR>==", MAP_KEY_OPTS)
+MAP_KEY("v", "<S-A-j>", ":<C-u>silent! '<,'>m '>+1<CR>gv", MAP_KEY_OPTS)
+MAP_KEY("v", "<S-A-k>", ":<C-u>silent! '<,'>m '<-2<CR>gv", MAP_KEY_OPTS)
 
 -- Duplicate lines
-map("n", "<S-A-d>", "<cmd>t.<cr>", opts)
-map("v", '<S-A-d>', "y'>p'[V']", opts)
+MAP_KEY("n", "<S-A-d>", "<cmd>t.<cr>", MAP_KEY_OPTS)
+MAP_KEY("v", '<S-A-d>', "y'>p'[V']", MAP_KEY_OPTS)
 
 -- Lazy
-map("n", "<leader>L", "<cmd>Lazy<cr>", opts)
+MAP_KEY("n", "<leader>L", "<cmd>Lazy<cr>", MAP_KEY_OPTS)
 
 -- Window nav
-map("n", "<A-C-j>", "<C-w>j", opts)
-map("n", "<A-C-k>", "<C-w>k", opts)
-map("n", "<A-C-l>", "<C-w>l", opts)
-map("n", "<A-C-h>", "<C-w>h", opts)
-map("n", "<A-C-i>", "<cmd>vertical resize +5<cr>", opts)
-map("n", "<A-C-o>", "<cmd>vertical resize -5<cr>", opts)
-map("n", "<A-C-e>", "<cmd>horizontal resize +2<cr>", opts)
-map("n", "<A-C-w>", "<cmd>horizontal resize -2<cr>", opts)
+MAP_KEY("n", "<A-C-j>", "<C-w>j", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-k>", "<C-w>k", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-l>", "<C-w>l", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-h>", "<C-w>h", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-i>", "<cmd>vertical resize +5<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-o>", "<cmd>vertical resize -5<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-e>", "<cmd>horizontal resize +2<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-C-w>", "<cmd>horizontal resize -2<cr>", MAP_KEY_OPTS)
 
 -- Qf nav
-map("n", "<S-A-o>", "<cmd>cnext<cr>", opts)
-map("n", "<S-A-u>", "<cmd>cprev<cr>", opts)
+MAP_KEY("n", "<S-A-o>", "<cmd>cnext<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-u>", "<cmd>cprev<cr>", MAP_KEY_OPTS)
 
 -- Tab nav 
-map("n", "<S-A-h>", "<cmd>tabprev<cr>", opts)
-map("n", "<S-A-l>", "<cmd>tabnext<cr>", opts)
-map("n", "<leader>tq", "<cmd>tabclose<cr>", opts)
-map("n", "<leader>tn", "<cmd>tab split<cr>", opts)
-map("n", "<leader>to", "<cmd>tabonly<cr>", opts)
-map("n", "<leader>ts", "<cmd>vs | Scratch %<cr>", opts)
+MAP_KEY("n", "<S-A-h>", "<cmd>tabprev<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-l>", "<cmd>tabnext<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>tq", "<cmd>tabclose<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>tn", "<cmd>tab split<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>to", "<cmd>tabonly<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>ts", "<cmd>vs | Scratch %<cr>", MAP_KEY_OPTS)
 
 -- Telescope
 local ok, telescope = pcall(require, "telescope.builtin")
@@ -80,99 +80,99 @@ if ok then
     mapTelescopeNV("<leader>ff", telescope.find_files)
     mapTelescopeNV("<leader>fg", telescope.live_grep)
     mapTelescopeNV("<leader>fh", telescope.help_tags)
-    map("n", "<leader>fp", telescope.pickers)
-    map("n", "<A-e>", "<cmd>Telescope recent_files<cr>", opts)
-    map("n", "<leader>fr", telescope.resume, opts)
-    map("n", "<C-l>", telescope.git_status, opts)
-    map("n", "<leader>ks", telescope.git_stash, opts)
-    map("n", "<leader>kb", telescope.git_branches, opts)
-    map("n", "<leader>fw", "<cmd>Telescope current_buffer_fuzzy_find fuzzy=false case_mode=ignore_case<cr>", opts)
+    MAP_KEY("n", "<leader>fp", telescope.pickers)
+    MAP_KEY("n", "<A-e>", "<cmd>Telescope recent_files<cr>", MAP_KEY_OPTS)
+    MAP_KEY("n", "<leader>fr", telescope.resume, MAP_KEY_OPTS)
+    MAP_KEY("n", "<C-l>", telescope.git_status, MAP_KEY_OPTS)
+    MAP_KEY("n", "<leader>ks", telescope.git_stash, MAP_KEY_OPTS)
+    MAP_KEY("n", "<leader>kb", telescope.git_branches, MAP_KEY_OPTS)
+    MAP_KEY("n", "<leader>fw", "<cmd>Telescope current_buffer_fuzzy_find fuzzy=false case_mode=ignore_case<cr>", MAP_KEY_OPTS)
 
-    map("n", "<leader>FF", function() telescope.find_files({ cwd = current_dir() }) end, opts)
-    map("n", "<leader>FG", function() telescope.live_grep({ cwd = current_dir() }) end, opts)
+    MAP_KEY("n", "<leader>FF", function() telescope.find_files({ cwd = current_dir() }) end, MAP_KEY_OPTS)
+    MAP_KEY("n", "<leader>FG", function() telescope.live_grep({ cwd = current_dir() }) end, MAP_KEY_OPTS)
 end
 
 -- Harpoon
-map("n", "<A-h>", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', opts)
-map("n", "<A-n>", '<cmd>lua require("harpoon.mark").add_file()<cr>', opts)
-map("n", "<A-a>", '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', opts)
-map("n", "<A-s>", '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', opts)
-map("n", "<A-d>", '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', opts)
-map("n", "<A-f>", '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', opts)
-map("n", "<A-g>", '<cmd>lua require("harpoon.ui").nav_file(5)<cr>', opts)
-map("n", "<A-t>", '<cmd>lua require("harpoon.ui").nav_file(6)<cr>', opts)
+MAP_KEY("n", "<A-h>", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-n>", '<cmd>lua require("harpoon.mark").add_file()<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-a>", '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-s>", '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-d>", '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-f>", '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-g>", '<cmd>lua require("harpoon.ui").nav_file(5)<cr>', MAP_KEY_OPTS)
+MAP_KEY("n", "<A-t>", '<cmd>lua require("harpoon.ui").nav_file(6)<cr>', MAP_KEY_OPTS)
 
 -- Gitsings
-map("n", "<C-A-d>", "<cmd>Gitsigns next_hunk<cr>", opts)
-map("n", "<C-A-u>", "<cmd>Gitsigns prev_hunk<cr>", opts)
-map("n", "<S-A-n>", "<cmd>Gitsigns next_hunk<cr>", opts)
-map("n", "<S-A-p>", "<cmd>Gitsigns prev_hunk<cr>", opts)
-map("n", "<A-z>", "<cmd>Gitsigns reset_hunk<cr>", opts)
-map("n", "<leader>ss", "<cmd>Gitsigns preview_hunk<cr>", opts)
+MAP_KEY("n", "<C-A-d>", "<cmd>Gitsigns next_hunk<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-A-u>", "<cmd>Gitsigns prev_hunk<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-n>", "<cmd>Gitsigns next_hunk<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-p>", "<cmd>Gitsigns prev_hunk<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<A-z>", "<cmd>Gitsigns reset_hunk<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>ss", "<cmd>Gitsigns preview_hunk<cr>", MAP_KEY_OPTS)
 
 -- Terminal
-map("t", "<Esc>", "<C-\\><C-n>", opts)
-map("t", "<A-y>", "<C-\\><C-n><cmd>ToggleTerm<cr>", opts)
+MAP_KEY("t", "<Esc>", "<C-\\><C-n>", MAP_KEY_OPTS)
+MAP_KEY("t", "<A-y>", "<C-\\><C-n><cmd>ToggleTerm<cr>", MAP_KEY_OPTS)
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "json",
     callback = function()
-        map("v", "<leader>f", "<cmd>CocCommand formatJson.selected --indent=4<cr>", { buffer = true })
-        map("n", "<leader>fa", "<cmd>CocCommand formatJson --indent=4<cr>", { buffer = true })
-        map("n", "<leader>f", "<Nop>")
+        MAP_KEY("v", "<leader>f", "<cmd>CocCommand formatJson.selected --indent=4<cr>", { buffer = true })
+        MAP_KEY("n", "<leader>fa", "<cmd>CocCommand formatJson --indent=4<cr>", { buffer = true })
+        MAP_KEY("n", "<leader>f", "<Nop>")
     end
 })
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "qf",
     callback = function()
-        map("n", "<cr>", "<cr><cmd>copen<cr>", { buffer = true })
-        map("n", "q", "<cmd>cclose<cr>", { buffer = true })
-        map("n", "<A-CR>", "<cr><cmd>cclose<cr>", { buffer = true })
-        map("n", "<esc>", "<cmd>cclose<cr>", { buffer = true })
-        map("n", "x", "<cmd>.Reject<cr>", { buffer = true })
-        map("v", "x", ":Reject<cr>", { buffer = true })
+        MAP_KEY("n", "<cr>", "<cr><cmd>copen<cr>", { buffer = true })
+        MAP_KEY("n", "q", "<cmd>cclose<cr>", { buffer = true })
+        MAP_KEY("n", "<A-CR>", "<cr><cmd>cclose<cr>", { buffer = true })
+        MAP_KEY("n", "<esc>", "<cmd>cclose<cr>", { buffer = true })
+        MAP_KEY("n", "x", "<cmd>.Reject<cr>", { buffer = true })
+        MAP_KEY("v", "x", ":Reject<cr>", { buffer = true })
     end
 })
 
 -- LSP
-map("n", "gr", telescope.lsp_references, opts)
-map("n", "<A-p>", telescope.lsp_document_symbols, opts)
-map("n", "<leader>gi", telescope.lsp_implementations, opts)
+MAP_KEY("n", "gr", telescope.lsp_references, MAP_KEY_OPTS)
+MAP_KEY("n", "<A-p>", telescope.lsp_document_symbols, MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>gi", telescope.lsp_implementations, MAP_KEY_OPTS)
 
-map("n", "gd", vim.lsp.buf.definition, opts)
-map("n", "]]", vim.diagnostic.goto_next, opts)
-map("n", "[[", vim.diagnostic.goto_prev, opts)
-map("n", "<A-CR>", vim.lsp.buf.code_action, opts)
-map("v", "<A-CR>", vim.lsp.buf.code_action, opts)
-map("n", "<S-A-f>", vim.lsp.buf.format, opts)
-map("v", "<S-A-f>", vim.lsp.buf.format, opts)
-map("n", "<C-p>", vim.lsp.buf.signature_help, opts)
-map("i", "<C-p>", vim.lsp.buf.signature_help, opts)
-map("n", "<A-r>", vim.lsp.buf.rename, opts)
-map("n", "<leader>ah", vim.lsp.buf.hover, opts)
-map("n", "<leader>ls", "<cmd>LspStart<cr>", opts)
-map("n", "<leader>lr", "<cmd>LspRestart<cr>", opts)
+MAP_KEY("n", "gd", vim.lsp.buf.definition, MAP_KEY_OPTS)
+MAP_KEY("n", "]]", vim.diagnostic.goto_next, MAP_KEY_OPTS)
+MAP_KEY("n", "[[", vim.diagnostic.goto_prev, MAP_KEY_OPTS)
+MAP_KEY("n", "<A-CR>", vim.lsp.buf.code_action, MAP_KEY_OPTS)
+MAP_KEY("v", "<A-CR>", vim.lsp.buf.code_action, MAP_KEY_OPTS)
+MAP_KEY("n", "<S-A-f>", vim.lsp.buf.format, MAP_KEY_OPTS)
+MAP_KEY("v", "<S-A-f>", vim.lsp.buf.format, MAP_KEY_OPTS)
+MAP_KEY("n", "<C-p>", vim.lsp.buf.signature_help, MAP_KEY_OPTS)
+MAP_KEY("i", "<C-p>", vim.lsp.buf.signature_help, MAP_KEY_OPTS)
+MAP_KEY("n", "<A-r>", vim.lsp.buf.rename, MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>ah", vim.lsp.buf.hover, MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>ls", "<cmd>LspStart<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>lr", "<cmd>LspRestart<cr>", MAP_KEY_OPTS)
 
-map("n", "-", require("oil").open, opts)
-map("v", "<leader>fj", "!jq<cr>", opts)
+MAP_KEY("n", "-", require("oil").open, MAP_KEY_OPTS)
+MAP_KEY("v", "<leader>fj", "!jq<cr>", MAP_KEY_OPTS)
 
 -- Settings
-map("n", "<leader>sw", "<cmd>set wrap!<cr>", opts)
-map("n", "<leader>sp", "<cmd>silent! setlocal spell! spelloptions=camel spelllang=ru_yo,en_us<cr>", opts)
+MAP_KEY("n", "<leader>sw", "<cmd>set wrap!<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>sp", "<cmd>silent! setlocal spell! spelloptions=camel spelllang=ru_yo,en_us<cr>", MAP_KEY_OPTS)
 
 -- Alacritty specific mappings
-map("n", "<C-Left>", "<C-w>h", opts)
-map("n", "<C-Down>", "<C-w>j", opts)
-map("n", "<C-Up>", "<C-w>k", opts)
-map("n", "<C-Right>", "<C-w>l", opts)
-map("n", "<C-A-Right>", "<cmd>vertical resize +5<cr>", opts)
-map("n", "<C-A-Left>", "<cmd>vertical resize -5<cr>", opts)
-map("n", "<C-A-Up>", "<cmd>horizontal resize +2<cr>", opts)
-map("n", "<C-A-Down>", "<cmd>horizontal resize -2<cr>", opts)
+MAP_KEY("n", "<C-Left>", "<C-w>h", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-Down>", "<C-w>j", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-Up>", "<C-w>k", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-Right>", "<C-w>l", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-A-Right>", "<cmd>vertical resize +5<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-A-Left>", "<cmd>vertical resize -5<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-A-Up>", "<cmd>horizontal resize +2<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<C-A-Down>", "<cmd>horizontal resize -2<cr>", MAP_KEY_OPTS)
 
-map("n", "<leader>ip", "<cmd>echo expand('%:P')<cr>", opts)
-map("n", "<leader>iP", "<cmd>echo expand('%:p')<cr>", opts)
+MAP_KEY("n", "<leader>ip", "<cmd>echo expand('%:P')<cr>", MAP_KEY_OPTS)
+MAP_KEY("n", "<leader>iP", "<cmd>echo expand('%:p')<cr>", MAP_KEY_OPTS)
 
 vim.api.nvim_create_user_command('ExecuteToSplit', function()
     local cmd = vim.fn.getreg('"'):gsub('[\r\n]', ' ')
@@ -183,4 +183,4 @@ vim.api.nvim_create_user_command('ExecuteToSplit', function()
     vim.cmd('vsplit')
     vim.api.nvim_set_current_buf(scratch_buf)
 end, {})
-map('v', '<leader>E', 'y:ExecuteToSplit<CR>', opts)
+MAP_KEY('v', '<leader>E', 'y:ExecuteToSplit<CR>', MAP_KEY_OPTS)
---
+++ lua/stash_plugins/dap.lua
@@ -0,0 +1,53 @@
+return {
+    {
+        'mfussenegger/nvim-dap',
+        dependencies = {
+            'rcarriga/nvim-dap-ui',
+            'jay-babu/mason-nvim-dap.nvim',
+        },
+        cmd = { 'DapContinue', 'DapToggleBreakpoint' },
+    },
+    {
+        'rcarriga/nvim-dap-ui',
+        config = function()
+            local dap = require('dap')
+            local dapui = require('dapui')
+            dapui.setup({
+                layouts = {
+                    {
+                        elements = {
+                            { id = "console", size = 0.75 },
+                            { id = "scopes",  size = 0.25 },
+                        },
+                        position = "bottom",
+                        size = 15
+                    },
+                }
+            })
+            dap.listeners.after.event_initialized['dapui_config'] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated['dapui_config'] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited['dapui_config'] = function()
+                dapui.close()
+            end
+
+            MAP_KEY("n", "<C-_>", "<cmd>DapToggleBreakpoint<cr>", MAP_KEY_OPTS)
+            MAP_KEY("n", "<C-n>", "<cmd>DapContinue<cr>", MAP_KEY_OPTS)
+            MAP_KEY("n", "<C-u>", "<cmd>lua require('dapui').toggle()<cr>", MAP_KEY_OPTS)
+            MAP_KEY("n", "<A-/>", "<cmd>lua require('dapui').eval()<cr>", MAP_KEY_OPTS)
+        end
+    },
+    {
+        'jay-babu/mason-nvim-dap.nvim',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'nvim-neotest/nvim-nio'
+        },
+        opts = {
+            handlers = {}
+        },
+    }
+}
